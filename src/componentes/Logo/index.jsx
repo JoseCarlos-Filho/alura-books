40d57d1c39668d34b161c6669b5bb79aa32fdea6
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-08-19 21:20:03 -0300

    refactor: substituir div por componentes styled LogoContainer e LogoImage

--- src/componentes/Logo/index.jsx
+++ src/componentes/Logo/index.jsx
@@ -1,14 +1,23 @@
-import "./estilo.css";
 import logo from "../../assets/logo.svg";
+import styled from "styled-components";
+
+const LogoContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+`;
+
+const LogoImage = styled.img`
+  margin-right: 10px;
+`;
 
 function Logo() {
   return (
-    <div className="logo">
-      <img src={logo} className="App-logo" alt="logo" />
+    <LogoContainer>
+      <LogoImage src={logo} alt="logo" />
       <p>
         <strong>Alura</strong>Books
       </p>
-    </div>
+    </LogoContainer>
   );
 }
 
