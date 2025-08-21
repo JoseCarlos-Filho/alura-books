7f5cacb6de2966c1b45304659c0767df8bdec704
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-08-20 21:48:05 -0300

    feat: adicionar dados dos últimos lançamentos de livros

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLacamentos.jsx
@@ -0,0 +1,12 @@
+import livro from "../../assets/livro.png";
+
+export const livros = [
+  { nome: "Liderança em Design", src: livro, id: 1 },
+  { nome: "JavaScript Assertivo", src: livro, id: 2 },
+  {
+    nome: "Cangaceiro JavaScript: Uma aventura no sertão da programação",
+    src: livro,
+    id: 3,
+  },
+  { nome: "Apache Kafka e Spring Boot", src: livro, id: 4 },
+];
