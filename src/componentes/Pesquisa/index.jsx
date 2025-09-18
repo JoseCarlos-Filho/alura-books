c1cf7a2df20976f794bfcfb5881d3b35be73aa7d
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-09-17 22:16:29 -0300

    feat: implementar busca e adição de livros aos favoritos

--- src/componentes/Pesquisa/index.jsx
+++ src/componentes/Pesquisa/index.jsx
@@ -1,14 +1,17 @@
 import Input from "../Input";
 import styled from "styled-components";
-import { useState } from "react";
-import { livros } from "./dadosPesquisa";
+import { useEffect, useState } from "react";
+import { getLivros } from "../../servicos/livros";
+import { getFavoritos, postFavorito } from "../../servicos/favoritos";
 
 const PesquisaContainer = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
   color: #fff;
   text-align: center;
   padding: 85px 0;
-  height: 270px;
+  //height: 270px;
+  min-height: 270px;
+  max-height: 100%;
   width: 100%;
 `;
 
@@ -44,6 +47,45 @@ const Resultado = styled.div`
 
 function Pesquisa() {
   const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+  const [livros, setLivros] = useState([]);
+  const [favoritos, setFavoritos] = useState([]);
+
+  useEffect(() => {
+    fetchLivros();
+    fetchFavoritos();
+  }, []);
+
+  async function fetchFavoritos() {
+    try {
+      setFavoritos(await getFavoritos());
+    } catch (error) {
+      console.error("Falha ao buscar favoritos:", error);
+    }
+  }
+
+  async function fetchLivros() {
+    try {
+      const livrosDaAPI = await getLivros();
+      setLivros(livrosDaAPI);
+    } catch (error) {
+      console.error("Falha ao buscar livros:", error);
+    }
+  }
+
+  async function insertFavorito(id) {
+    if (favoritos.find((f) => f.id === id)) {
+      alert(`Este livro já está na sua lista de favoritos.`);
+      return;
+    }
+    try {
+      await postFavorito(id);
+      alert(`Livro adicionado aos favoritos!`);
+      await fetchFavoritos(); // Atualiza a lista de favoritos para refletir a mudança
+    } catch (error) {
+      const mensagem = error.response?.data?.message || error.message;
+      alert(`Erro ao adicionar favorito: ${mensagem}`);
+    }
+  }
 
   return (
     <PesquisaContainer>
@@ -61,7 +103,7 @@ function Pesquisa() {
         }}
       />
       {livrosPesquisados.map((livro) => (
-        <Resultado>
+        <Resultado onClick={() => insertFavorito(livro.id)}>
           <p>{livro.nome}</p>
           <img src={livro.src} />
         </Resultado>
